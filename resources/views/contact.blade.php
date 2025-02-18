12f5ba9d395dc600293f733964d58867fbcde8f9
Abby Rea <rea_humphrys-a@ulster.ac.uk>
2025-02-18 15:01:45 +0000

    update

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -6,7 +6,7 @@
   <h1> Hello fromt the contact page 
     it worked
   </h1>
-=======
+
   <h1> Hello from the contact page it worked </h1>
 >>>>>>> b2ecf3651dca2a7fbf5010e64c8e68a8c12987b2
 </x-layout>
