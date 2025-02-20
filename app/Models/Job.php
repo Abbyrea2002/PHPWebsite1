d324fb2b9843d5f8771133e384878e9ccdd7aab9
Abby Rea <rea_humphrys-a@ulster.ac.uk>
2025-02-20 15:31:33 +0000

    fillable error

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -1,19 +1,12 @@
 <?php
- 
- namespace App\Models;
- use Illuminate\Database\Eloquent\Model;
 
- class Job extends Model {
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Job extends Model
+{
     protected $table = 'job_listings';
-   
 
+    protected $fillable = ['title', 'salary'];
 }
-
-
-
-
-
-
-
-
- ?>
