38963738df526abc645dd5741fb054ac3670bdad
Abby Rea <rea_humphrys-a@ulster.ac.uk>
2025-02-25 19:39:53 +0000

    hasmany

--- app/Models/Employer.php
+++ app/Models/Employer.php
@@ -9,4 +9,9 @@ class Employer extends Model
 {
     /** @use HasFactory<\Database\Factories\EmployerFactory> */
     use HasFactory;
+
+    public function jobs(){
+        return $this->hasMany(Job::class);
+    }
 }
+\\https://laracasts.com/series/30-days-to-learn-laravel-11/episodes/12?autoplay=true
