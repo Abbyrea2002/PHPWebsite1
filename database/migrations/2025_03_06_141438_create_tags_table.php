08aafdca30052e99263ad3c630e7f49c7ef72ea7
Abby Rea <rea_humphrys-a@ulster.ac.uk>
2025-03-18 19:51:41 +0000

    update

--- database/migrations/2025_03_06_141438_create_tags_table.php
+++ database/migrations/2025_03_06_141438_create_tags_table.php
@@ -17,6 +17,12 @@ return new class extends Migration
             $table->timestamps();
         });
 
+       //pivot table - many to many relationship between jobs and tag table
+       //creating foreign key thaat links id in jobs table 
+       // job_listing_id connected to id in jobs table
+       //constrained - adds foreign key constraint that links to prim key of jobs table
+       //prevents invalid data insert and deleting job
+       //cascadeOnDelete - deletes record from job tag if u delete from jobs
         Schema::create('job_tag', function (Blueprint $table) {
             $table->id();
             $table->foreignIdFor(\App\Models\Job::class, 'job_listing_id')->constrained()->cascadeOnDelete();
---
+++ resources/views/jobs/create.blade.php
@@ -0,0 +1,12 @@
+<x-layout>
+    <x-slot:heading>
+         Create Job
+    </x-slot:heading>
+
+    
+    <p>
+        TODO
+    </p>
+
+
+</x-layout>
