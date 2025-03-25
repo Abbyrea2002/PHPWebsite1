a5c774965ffa69227121d2a584be0023a43bec9e
Abby Rea <rea_humphrys-a@ulster.ac.uk>
2025-03-25 16:55:21 +0000

    update

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,11 @@ use Illuminate\Support\Facades\Route;
 use App\Models\Job;
 
 
+Route::get('/', function () {
+   
+    return view('home');
 
+});
 Route::get('/home', function () {
    
     return view('home');
@@ -13,7 +17,7 @@ Route::get('/home', function () {
 });
 
 Route::get('/jobs', function ()  {
-    $jobs = Job::with('employer')->simplePaginate(3);
+    $jobs = Job::with('employer')->latest()->simplePaginate(3);
 
     return view('jobs.index',[
          'jobs' => $jobs
@@ -29,6 +33,17 @@ Route::get('/jobs/{id}', function ($id) {
    $job = Job::find($id);
         
     return view('jobs.show', ['job' => $job]);
+});
+
+Route::post('/jobs', function () {
+    //validation
+    Job::create([
+        'title' => request('title'),
+        'salary' => request('salary'),
+        'employer_id' => 1
+    ]);
+
+    return redirect('/jobs');
 });
 
 Route::get('/contact', function () {
