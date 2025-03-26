5351582da6b7d2c27aa593ef2b518824dbe905b1
Abby Rea <rea_humphrys-a@ulster.ac.uk>
2025-03-26 13:33:50 +0000

    week 9

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -7,7 +7,7 @@
     <p>
     <form method="POST" action="/jobs">
         @csrf
-        
+
   <div class="space-y-12">
     <div class="border-b border-gray-900/10 pb-12">
       <h2 class="text-base/7 font-semibold text-gray-900">Create a new job</h2>
@@ -22,7 +22,7 @@
             </div>
           </div>
         </div>
-
+    </div>
         <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
         <div class="sm:col-span-4">
           <label for="salary" class="block text-sm/6 font-medium text-gray-900">Salary</label>
@@ -31,7 +31,22 @@
               <input type="text" name="salary" id="salary" class="block min-w-0 grow py-1.5 px-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="£50,000">
             </div>
           </div>
+
+        <!-- <div class="mt-10">
+             @if($errors->any())
+             <ul> 
+            @foreach($errors->all() as $error)
+            <li class="text-red-500 italic"> {{ $error }} </li>
+            @endforeach
+             </ul>
+           @endif
+        </div> -->
         </div>
+    </div>
+
+   
+ 
+
   <div class="mt-6 flex items-center justify-end gap-x-6">
     <button type="button" class="text-sm/6 font-semibold text-gray-900">Cancel</button>
     <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save</button>
