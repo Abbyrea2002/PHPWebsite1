a8cc2fb54dfff8bf8846063b5cafcc3e70bae62f
Abby Rea <rea_humphrys-a@ulster.ac.uk>
2025-04-03 11:19:45 +0100

    fixed

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+
+use Illuminate\Validation\Rules\Password;
 
 class RegisterController extends Controller
 {
